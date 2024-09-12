6a07fe7c44364ad554d3295dfac8013d3d196a2e
BloodMoonLuna <mmichiaels98@gmail.com>
2024-09-12 14:29:02 -0400

    first commit

---
+++ Showcase_Your_Dance_Moves!.py
@@ -0,0 +1,17 @@
+    #Python Indentation Practice with if Statements
+    #Objective: The aim of this assignment is to understand the importance od indentation in Python, especially with if statements.
+    #Task 1: Code Correction, Below is a piece of Python code with incorrect indentation. Your task is to correct it.
+
+weather = "sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else: 
+    print:("Take an umbrella!")
+
+    #Task 2: Your Mood Today, Ask the user how they are feeling today. If they say "happy", print "That's great to hear!", ? 
+    # if they say "sad", print "I hope your day gets better!". Ensure your if statm,ent is properly indented.
+user_mood = input("How are you feeling today?")
+if user_mood == "happy":
+    print("That's greast to hear!")
+if user_mood == "sad":
+    print("Ihope your day gets better!")
