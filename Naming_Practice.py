202f31d5b0e37223a0a3537292350e16f5fc6387
BloodMoonLuna <mmichiaels98@gmail.com>
2024-09-16 17:08:48 -0400

    Naming_Practice

---
+++ Naming_Practice.py
@@ -0,0 +1,9 @@
+#Objective: The aim of this assignment is to understand and apply Python's naming conventions, particuraly for variables and constants./ 
+#Task 1: Code Correction, You have been given a piece of code with several variable and constant names that don't follow the Python naming convention./ 
+#Your task is to correct them:
+
+PI_VALUE = 3.14
+user_age = 25
+userLocation = "New York"
+maxLimit = 1000
+print(user_age)
